ccc9a7e25416c034bf3d1eaffd736318d2f8b9d8
Iurii Pouektov <poluektov.yuriy@gmail.com>
2021-09-03 19:05:18 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by yuriy.p on 03.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
